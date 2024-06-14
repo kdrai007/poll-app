a6a3714c8c9341b9abccc43c1f055ca7cd1b0558
kundan rai <987kundanrai@gmail.com>
2024-06-14 14:15:07 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import { NavBar } from './components/NavBar'
+
+function App() {
+
+
+  return (
+    <>
+      <div >
+        <NavBar />
+        <p className='text-red-500'> hello there</p>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,7 @@
+export const NavBar = () => {
+  return (
+    <div>
+      this is navBar
+    </div>
+  )
+}
